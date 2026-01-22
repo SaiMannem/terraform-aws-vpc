06ee333df2cda98a601edadf0498ddd1df742f83
sai mannem <mannemsaikrishna1997@gmail.com>
2026-01-22 19:46:03 +0530

    configFiles

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ locals {
 resource "aws_vpc" "this" {
   count = var.vpc_config.create_vpc ? 1 : 0
 
-  cidr_block       = var.vpc_config.cidr_block
-  instance_tenancy = var.vpc_config.instance_tenancy
-
+  cidr_block           = var.vpc_config.cidr_block
+  instance_tenancy     = var.vpc_config.instance_tenancy
+  
   enable_dns_support   = var.vpc_config.enable_dns_support
   enable_dns_hostnames = var.vpc_config.enable_dns_hostnames
 
