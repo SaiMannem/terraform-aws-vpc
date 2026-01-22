011d0f3239fd698716ec70eb6d4071c0ba65b55d
sai mannem <mannemsaikrishna1997@gmail.com>
2026-01-22 19:51:12 +0530

    configFiles

--- main.tf
+++ main.tf
@@ -12,12 +12,10 @@ resource "aws_vpc" "this" {
 
   cidr_block           = var.vpc_config.cidr_block
   instance_tenancy     = var.vpc_config.instance_tenancy
-  
   enable_dns_support   = var.vpc_config.enable_dns_support
   enable_dns_hostnames = var.vpc_config.enable_dns_hostnames
 
   tags = merge(local.common_tags, {
     Name = var.vpc_config.name
   })
-
 }
